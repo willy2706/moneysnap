c5b948e79cfd795f3c69c28d60c13178a881c8c7
willy2706 <willylie2706@gmail.com>
2015-10-07 22:46:52 +0700

    fab preparation

--- moneysnapapp/src/main/java/com/moneysnap/app/MainActivity.java
+++ moneysnapapp/src/main/java/com/moneysnap/app/MainActivity.java
@@ -1,30 +1,44 @@
 package com.moneysnap.app;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends Activity {
-
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ListView listView = (ListView) findViewById(android.R.id.list);
+//        fab.attachToListView(listView);
         setContentView(R.layout.activity_main);
+//        fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setType(FloatingActionButton.TYPE_MINI);
+//        fab.show();
+//        Display display = getWindowManager().getDefaultDisplay();
+//        fab.layout();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println("haha");
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        fab.setOnClickListener(null);
+//    }
 }
