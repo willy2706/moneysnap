de728a8c1f71388d939a36744f210d32a1eb87eb
willy2706 <willylie2706@gmail.com>
2015-10-07 11:58:59 +0700

    multiple image for background

--- moneysnapapp/src/main/java/com/moneysnap/app/MainActivity.java
+++ moneysnapapp/src/main/java/com/moneysnap/app/MainActivity.java
@@ -2,7 +2,6 @@ package com.moneysnap.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 
@@ -12,14 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
